e1c8bd7b2cace3e5716f80e1186d77d4fe8e2995
shaojung <shaojung@gmail.com>
2016-12-06 19:24:17 +0800

    Fragment Replace

--- app/src/main/java/com/example/yvtc/yvn120601/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn120601/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.yvtc.yvn120601;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickA(View v)
+    {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.linearLayout1, new FragmentA(), "f_a");
+        ft.commit();
+    }
+
+    public void clickB(View v)
+    {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.linearLayout1, new FragmentB(), "f_b");
+        ft.commit();
     }
 }
